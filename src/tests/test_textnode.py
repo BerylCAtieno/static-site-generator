2c42d2183ea3401ec26377dfd635f39659945f4f
BerylCAtieno <berylatieno30@gmail.com>
2024-06-27 11:35:57 +0300

    reorganize tests

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -9,6 +9,12 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
+    def test_repr(self):
+        node_obj = TextNode("The text", "italic", "link.com")
+        expected_repr = 'TextNode("The text", "italic", "link.com")'
+
+        self.assertEqual(repr(node_obj), expected_repr)
+
 
 if __name__ == "__main__":
     unittest.main()
